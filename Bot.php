1c7547b099bc3adae553ccbb573e9354d245d585
Farshad <farshad.tofighi74@gmail.com>
2020-06-04 18:05:00 +0430

    add command examples and new helper functions

--- Bot.php
+++ Bot.php
@@ -8,40 +8,45 @@ class Bot extends Api
 
 
 
-
     use Functions;
     public $message;
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public function __construct()
     {
         set_time_limit(0);
         error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
         date_default_timezone_set('Asia/Tehran');
-        @DB::connect('localhost' , 'root' , '' , 'digital_coins');
-        $this->token = "496504159:AAGjs_ud4woiyNBT3pXNXt2kCt_cP0ovYIM";
+        @DB::connect('localhost' , 'Username' , 'Password' , 'DB_Name');
+        $this->token = "Bot_Token";
     }
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public function Run()
     {
         $messages = $this->getUpdates();
         foreach($messages as $this->message)
         {
-            $last_update_id = $this->message['update_id'];
+            $last_update_id = $this->message->update_id;
 
-            if(isset($this->message['message']))
+            if(isset($this->message->message))
             {
-                exit($this->message['chat']['id']);
-                $this->message = $this->message['message'];
-                $this->sendMessage($this->message['chat']['id'], 'hi');
+                // when user send message
+                $this->message = $this->message->message;
+
+                if($this->message->text == '/start')
+                    $this->start_command();
+                else if($this->message->text == '/help')
+                    $this->help_command();
             }
-            else if(isset($this->message['edited_message']))
+            else if(isset($this->message->edited_message))
             {
-                $this->message = $this->message['edited_message'];
+                // when user edit his message
+                $this->message = $this->message->edited_message;
                 // code here
             }
-            else if(isset($this->message['callback_query']))
+            else if(isset($this->message->callback_query))
             {
-                $this->message = $this->message['callback_query'];
+                // when user click on inline keyboard
+                $this->message = $this->message->callback_query;
                 // code here
             }
 
@@ -49,11 +54,49 @@ class Bot extends Api
             file_put_contents('update_id.txt' , $last_update_id);
         }
     }
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function start_command()
+    {
+        $text = "start command";
+        // button keyboard
+        $keyboard1 =
+        [
+            [
+                [ 'text' => 'Button 1' ],
+                [ 'text' => 'Button 2' ]
+            ],
+            [
+                [ 'text' => 'Button 3' ],
+            ],
+        ];
+        // inline keyboard
+        $keyboard2 =
+        [
+            [
+                [ 'text' => 'Button', 'callback_data' => 'data' ],
+            ],
+            [
+                [ 'text' => 'farshadth.ir', 'url' => 'https://farshadth.ir' ],
+            ],
+        ];
+        $keyboard = $this->buttonKeyboard($keyboard1);
+//        $keyboard = $this->inlineKeyboard($keyboard2); // use for send inline keyboard instead
+        $optional = "&reply_markup=$keyboard";
+        $this->sendMessage($this->message->chat->id, $text, $optional);
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function help_command()
+    {
+        $text = "help command";
+        $this->sendMessage($this->message->chat->id, $text);
+        $this->saveState($this->message->chat->id, "user_is_in_help_command"); // save state of user for conversation
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 $obj = new Bot();
 while (true)
 {
--- Functions.php
+++ Functions.php
@@ -7,10 +7,88 @@ trait Functions
 
 
 
-    public function __construct()
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function callerFunction()
     {
-        // code here
+        // return caller function
+        $trace = debug_backtrace();
+        return $trace[2]['function'];
     }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function buttonKeyboard($rows, $optional = null)
+    {
+        $keyboard =
+        [
+            'keyboard' => $rows,
+            'resize_keyboard' => true,
+            $optional
+        ];
+        $keyboard = json_encode($keyboard);
+        return $keyboard;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function inlineKeyboard($rows)
+    {
+        $keyboard =
+        [
+            'inline_keyboard' => $rows
+        ];
+        $keyboard = json_encode($keyboard);
+        return $keyboard;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function saveState($chat_id, $state)
+    {
+        /**
+         * create table in your database with info in below
+         * table name: states
+         * columns:
+         * id: Primary Key, Auto Increament
+         * chat_id: INT, UNIQUE
+         * state: VARCHAR
+         */
+        // save state of user to start conversation
+        $result = @DB::insert("INSERT INTO states (chat_id, state) VALUES (? , ?)", [ $chat_id, $state ] );
+        return $result;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function getState($chat_id)
+    {
+        // get state of user when is in conversation
+        $state = @DB::select("SELECT state FROM states WHERE chat_id = ?", [ $chat_id ] );
+        if($state->num_rows > 0)
+            while ($row = $state->fetch_object())
+                return $row->state;
+        else
+            return false;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function updateState($chat_id, $state)
+    {
+        // update state of user when answers to conversation
+        $result = @DB::update("UPDATE states SET state = ? WHERE chat_id = ? LIMIT 1", [ $state, $chat_id ] );
+        return $result;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function deleteState($chat_id)
+    {
+        // delete state of user when conversation is finished
+        $result = @DB::delete("DELETE FROM states WHERE chat_id = ? LIMIT 1", [ $chat_id ] );
+        return $result;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function updateOrSaveState($chat_id, $state)
+    {
+        // update state of user in conversation if saved before else save it
+        $check_state = @DB::select("SELECT state FROM states WHERE chat_id = ? LIMIT 1", [ $chat_id ] );
+        if($check_state->num_rows > 0)
+            $result = @DB::update("UPDATE states SET state = ? WHERE chat_id = ? LIMIT 1", [ $state, $chat_id ] );
+        else
+            $result = @DB::insert("INSERT INTO states (chat_id, state) VALUES (? , ?)", [ $chat_id, $state ] );
+
+        return $result;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
